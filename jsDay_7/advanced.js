649d63aad27baa78feee71be186b9d731c358bba
Miguel Perello garcia <canyavall@gmail.com>
2017-05-03 10:42:59 +0200

    Save Last day exercises

--- jsDay_7/advanced.js
+++ jsDay_7/advanced.js
@@ -1,11 +1,13 @@
+/**
+ * MyBind function
+ */
+
 // function myBind(fn, obj){
-//       var arg3 = arguments[2];
+//       var mainArgs = [].slice.call(arguments,2);
 //   return function (){
-//       //console.log(arg3);
-//       var args = Array.prototype.slice.call(arguments);
-//       args.push(arg3);
-//       console.log(args);
-//     fn.apply(obj, args)
+//       var args = [].slice.call(arguments);
+//       var finalArgs = mainArgs.concat(args);
+//     fn.apply(obj, finalArgs)
 //   };
 // }
 //
@@ -16,7 +18,6 @@
 // function printName() {
 //   console.log('Thy name is: ' + this.name);
 // }
-//
 //
 // function greetingsTo(name) {
 //   console.log('Hello ' + name + ', my name is: ' + this.name);
@@ -41,13 +42,57 @@
 //
 // var boundAndFirst = myBind(greetingsToAll, obj, 'Monti');
 // boundAndFirst('Hornby');
+
+/**
+ * Currying Calculator
+ */
+// function createCurryCalc () {
+//   var mainArgs = [].slice.call(arguments);
+//     return function(){
+//       var args = [].slice.call(arguments);
+//       var totalArgs = args.concat(mainArgs);
+//       if (totalArgs.length >= 5){
+//         return totalArgs.reduce(function (a,b){return a*b});
+//       }
+//       return createCurryCalc.apply(this, totalArgs);
 //
+//     };
+// };
+// var curryCalc = createCurryCalc();
+// var partial = curryCalc(2, 3, 4);
+// console.log(partial(1, 3));
+//
+// var curryCalc2 = createCurryCalc();
+// var partial2 = curryCalc2(2);
+// partial2 = partial2(4, 5)
+// console.log(partial2(1, 3));
 
-var curryCalc = createCurryCalc();
-var partial = curryCalc(2, 3, 4);
-console.log(partial(1, 3));
+/**
+ * Curry function
+ */
 
-var curryCalc2 = createCurryCalc();
-var partial2 = curryCalc2(2);
-partial2 = partial2(4, 5)
-console.log(partial2(1, 3));
+ // function curry () {
+ //   var mainArgs = [].slice.call(arguments);
+ //     return function(){
+ //       var args = [].slice.call(arguments);
+ //       var totalArgs = args.concat(mainArgs);
+ //       return curry.apply(this, totalArgs);
+ //     };
+ // };
+
+/**
+ * Mynew
+ */
+ // function Person(name) {
+ //   this.name = name;
+ // }
+ //
+ // function myNew(ConstrucorFn) {
+ //   var obj = Object.create(ConstrucorFn.prototype);
+ //   var args = [].slice.call(arguments, 1);
+ //   ConstrucorFn.apply(obj, args);
+ //   return obj;
+ // }
+ //
+ // var manel = myNew(Person, 'Manel');
+ // console.log(manel);

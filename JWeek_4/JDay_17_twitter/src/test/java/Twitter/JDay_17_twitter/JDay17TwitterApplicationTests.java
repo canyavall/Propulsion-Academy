d099c29507fc31a51fecca3c544f8a7d9a1556e1
Miguel Perello garcia <canyavall@gmail.com>
2017-05-30 14:55:23 +0200

    Starting with boot spring

---
+++ JWeek_4/JDay_17_twitter/src/test/java/Twitter/JDay_17_twitter/JDay17TwitterApplicationTests.java
@@ -0,0 +1,16 @@
+package Twitter.JDay_17_twitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JDay17TwitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

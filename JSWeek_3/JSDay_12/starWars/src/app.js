946ce943d3a1d435a40c5722288408e8b94217a9
Miguel Perello garcia <canyavall@gmail.com>
2017-06-07 08:23:06 +0200

    star wars new way for rest

--- JSWeek_3/JSDay_12/starWars/src/app.js
+++ JSWeek_3/JSDay_12/starWars/src/app.js
@@ -1,68 +1,69 @@
-const myReady = () => {
-  let filmData = "";
-  let starShipsData = [];
-  let pilots = [];
+async function myReady () {
 
   //Get movie data from API
-  fetch("http://swapi.co/api/films?format=json")
-  .then((res)=> {
-    return res.json();
-  }).then((data)=>{
-    filmData = data;
-    data.results.forEach((movie, i)=>{
-      $("#movieSelect").append('<option value="'+i+'">'+movie.title+'</option>');
-    })
-  });
+  let data = await fetch("http://swapi.co/api/films?format=json");
+  if(data.status != 200) return "error";
+  let res = await data.json();
+  let filmData = res.results;
+
+  //Get airship data from API
+  data = await fetch("http://swapi.co/api/starships?format=json");
+  if(data.status != 200) return "error";
+  res = await data.json();
+  let starShipsData = res.results;
+console.log(res);
+  //Print movies
+  filmData.forEach((movie, i)=>{
+    $("#movieSelect").append('<option value="'+i+'">'+movie.title+'</option>');
+  })
 
   //Prepare movie selector
   $('#movieSelect').change(function()	{
     //Clean the starship selector
     $('#starshipSelect')
-    .find('option')
-    .remove()
-    .end()
-    .append('<option value="-1">Select One</option>')
+      .find('option')
+      .remove()
+      .end()
+      .append('<option value="-1">Select One</option>');
 
     //Get the info from the movie
 		let movieSelected = $(this).find('option:selected').val();
     let text = '';
-    console.log(filmData.results[movieSelected]['starships']);
-    for (let key in filmData.results[movieSelected]) {
+    for (let key in filmData[movieSelected]) {
       if (key != 'characters' && key != 'planets' &&
           key != 'starships' && key != 'vehicles' &&
           key != 'species' && key != 'url')
-      text += "<b>" + key + "</b>: " + filmData.results[movieSelected][key] + "<br \><br \>";
+      text += "<b>" + key + "</b>: " + filmData[movieSelected][key] + "<br \><br \>";
     }
-
     //Add to info
     $('#movieInfo').html(text);
 
-    //Prepare the list of startships
-    fetch("http://swapi.co/api/starships?format=json")
-    .then((res)=> {
-      return res.json();
-    }).then((data)=>{
-      starShipsData = data;
-      data.results.forEach((starship, i)=>{
-        if (filmData.results[movieSelected]['starships'].indexOf(starship.url) > -1)
-        $("#starshipSelect").append('<option value="'+i+'">'+starship.name+'</option>');
-      })
-    });
-	});
+    //Prepare starship data select
+    console.log(filmData[movieSelected]['starships'].length + "/ " + starShipsData.length);
+    starShipsData.forEach((starship, i)=>{
+      console.log(starship['url']);
+          if (filmData[movieSelected]['starships'].indexOf(starship['url']) > -1){
+            console.log("po");
+            $("#starshipSelect").append('<option value="'+i+'">'+starship.name+'</option>');
+          }
 
-  $('#starshipSelect').change(function()	{
-      //Get the info from the starship
-    let starshipSelected = $(this).find('option:selected').val();
-    let text = '';
-    for (let key in starShipsData.results[starshipSelected]) {
-      if (key != 'films' && key != 'pilots' )
-      text += "<b>" + key + "</b>: " + starShips.results[starshipSelected][key] + "<br \><br \>";
-    }
+        })
 
-    //Add to info
-    $('#starshipInfo').html(text);
-	});
+    $('#starshipSelect').change(function()	{
+        //Get the info from the starship
+      let starshipSelected = $(this).find('option:selected').val();
+      let text = '';
+      for (let key in starShipsData[starshipSelected]) {
+        if (key != 'films' && key != 'pilots' )
+        text += "<b>" + key + "</b>: " + starShipsData[starshipSelected][key] + "<br \><br \>";
+      }
 
-}
+      //Add to info
+      $('#starshipInfo').html(text);
+	  });
+  });
+};
 
-$(myReady);
+$(() => {
+  myReady();
+});

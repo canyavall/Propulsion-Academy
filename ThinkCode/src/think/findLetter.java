de6b1ec1c5eb5026ff29e047630845a823bd2086
Miguel Perello garcia <canyavall@gmail.com>
2017-06-12 08:24:02 +0200

    Auto stash before merge of "master" and "origin/master"

--- ThinkCode/src/think/findLetter.java
+++ ThinkCode/src/think/findLetter.java
@@ -1,23 +1,25 @@
 package think;
 
-public class findMissingLetter{
+public class findLetter{
 	
  public static void main(String[] args) {
-	 System.out.println(findMissingLetter(new char[] { 'a','b','c','d','f' }));
-	 System.out.println(findMissingLetter(new char[] { 'A','B','C','D','F' }));
+	 System.out.println(findMissingLetter(new char[] { 'a','b','c','d','f' })); //e
+	 System.out.println(findMissingLetter(new char[] { 'A','B','C','D','F' })); //E
+	 System.out.println(findMissingLetter(new char[] { 'm','n','o','q','r' })); //p
 }
   public static char findMissingLetter(char[] array){
 	  String abc = "abcdefghijklmnopqrstuvwxyz";
 	  char[] charArray = abc.toCharArray();
 	  int control = 0;
 	  for (int i = 0; i < charArray.length; i++) {
-		if (Character.toUpperCase(charArray[i]) != Character.toUpperCase(array[control])){
+		if (Character.toUpperCase(charArray[i]) == Character.toUpperCase(array[control])){
+			control++;	
+		}else{
 			if (control != 0) {
 				if (Character.isUpperCase(array[control])) return Character.toUpperCase(charArray[i]);
 				return charArray[i];
-			}			
-		}
-		control++;
+			}
+		}		
 	}	  
     return ' ';
   }
